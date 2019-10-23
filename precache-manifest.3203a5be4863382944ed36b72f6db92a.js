8ee2f0d9b414b1dab05332f32a3364cd79139c26
DevLinh <DevLinh99QB@gmail.com>
2019-10-23 10:26:33 +0700

    Updates

---
+++ precache-manifest.3203a5be4863382944ed36b72f6db92a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4852218f92c86c8404642665a0cd84d",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "9fe3db5a6c6006049f3e",
+    "url": "/recipe-search/static/css/main.48ae398b.chunk.css"
+  },
+  {
+    "revision": "6a2d8d765890e765fc19",
+    "url": "/recipe-search/static/js/2.f8e19372.chunk.js"
+  },
+  {
+    "revision": "9fe3db5a6c6006049f3e",
+    "url": "/recipe-search/static/js/main.12007504.chunk.js"
+  },
+  {
+    "revision": "c1af095eb005a3859578",
+    "url": "/recipe-search/static/js/runtime-main.6f74369e.js"
+  }
+]);
